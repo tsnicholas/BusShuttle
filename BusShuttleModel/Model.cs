3b4a139bece4102f758bbb03e1bea8f931ac43ae
tsnicholas <tsnicholas@bsu.edu>
2024-03-28 16:23:19 -0400

    Create Domain Model

---
+++ BusShuttleModel/Model.cs
@@ -0,0 +1,54 @@
+﻿namespace BusShuttleModel;
+
+public class Bus
+{
+    public int Id { get; set; }
+    public int BusNumber { get; set; }
+}
+
+public class Driver
+{
+    public int Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+}
+
+public class Loop
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+}
+
+public class Route
+{
+    public int Id { get; set; }
+    public int Order { get; set; }
+}
+
+public class Stop
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public double Latitude { get; set; }
+    public double Longitude { get; set; }
+    public int RouteId { get; set; }
+}
+
+public class Loop
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public List<Route> Routes { get; set; } 
+}
+
+public class Entry
+{
+    public int Id { get; set; }
+    public DateTime Timestamp { get; set; }
+    public int Boarded { get; set; }
+    public int LeftBehind { get; set; }
+    public int BusId { get; set; }
+    public int DriverId { get; set; }
+    public int LoopId { get; set; }
+    public int StopId { get; set; }
+}

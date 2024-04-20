7eddaaa6eb45be4ca1adeb93c0aca9fb3a93b940
tsnicholas <tsnicholas@bsu.edu>
2024-04-20 17:39:05 -0400

    Create Unit Tests for Account Service

---
+++ App.Tests/Service/AccountServiceTest.cs
@@ -0,0 +1,65 @@
+using Moq;
+using App.Service;
+using Microsoft.AspNetCore.Identity;
+using System.Security.Claims;
+namespace App.Tests.Service;
+
+public class AccountServiceTests
+{
+    #pragma warning disable CS8625 // Cannot convert null literal to non-nullable reference type.
+    private static readonly Mock<IUserStore<IdentityUser>> mockUserStore = new();
+    private static readonly Mock<IUserEmailStore<IdentityUser>> mockEmailStore = new();
+    private static readonly Mock<UserManager<IdentityUser>> mockUserManager = 
+        new(mockUserStore.Object, null, null, null, null, null, null, null, null);
+    private readonly AccountService service;
+    #pragma warning restore CS8625 // Cannot convert null literal to non-nullable reference type.
+
+    public AccountServiceTests()
+    {
+        service = new AccountService(mockUserManager.Object, mockUserStore.Object, mockEmailStore.Object);
+    }
+
+    [Fact]
+    public async void AccountService_CreateDriverAccount_Successfully()
+    {
+        const string driverEmail = "Dummy2377@outlook.com";
+        const string driverPassword = "Clever Password";
+        Claim expectedClaim = new("activated", "True");
+        mockUserManager.Setup(x => x.AddClaimAsync(It.IsAny<IdentityUser>(), expectedClaim))
+            .Returns(Task.FromResult(IdentityResult.Success));
+        mockUserManager.Setup(x => x.AddToRoleAsync(It.IsAny<IdentityUser>(), "Driver"))
+            .Returns(Task.FromResult(IdentityResult.Success));
+        mockUserStore.Setup(x => x.SetUserNameAsync(It.IsAny<IdentityUser>(), driverEmail, CancellationToken.None))
+            .Returns(Task.CompletedTask);
+        mockEmailStore.Setup(x => x.SetEmailAsync(It.IsAny<IdentityUser>(), driverEmail, CancellationToken.None))
+            .Returns(Task.CompletedTask);
+        mockUserManager.Setup(x => x.CreateAsync(It.IsAny<IdentityUser>(), driverPassword))
+            .Returns(Task.FromResult(IdentityResult.Success));
+        await service.CreateDriverAccount(driverEmail, driverPassword);
+    }
+
+    [Theory]
+    [InlineData([false])]
+    [InlineData([true])]
+    public async void AccountService_UpdateAccountActivation_Successfully(bool result)
+    {
+        const string driverEmail = "tsnicholas@bsu.edu";
+        var user = Activator.CreateInstance<IdentityUser>();
+        mockUserManager.Setup(x => x.FindByEmailAsync(driverEmail)).Returns(Task.FromResult(user));
+        IList<Claim> userClaims = [new Claim("activated", "False")];
+        mockUserManager.Setup(x => x.GetClaimsAsync(user)).Returns(Task.FromResult(userClaims));
+        mockUserManager.Setup(x => x.ReplaceClaimAsync(user, userClaims[0], new("activated", result.ToString())))
+            .Returns(Task.FromResult(IdentityResult.Success));
+        await service.UpdateAccountActivation(driverEmail, result);
+    }
+
+    [Fact]
+    public async void AccountService_GetCurrentEmail_Successfully()
+    {
+        IdentityUser? user = Activator.CreateInstance<IdentityUser>();
+        ClaimsPrincipal securityInformation = new();
+        mockUserManager.Setup(x => x.GetUserAsync(securityInformation)).Returns(Task.FromResult(user));
+        mockUserManager.Setup(x => x.GetEmailAsync(user)).Returns(Task.FromResult("Your Email"));
+        await service.GetCurrentEmail(securityInformation);
+    }
+}
--- App/Service/AccountService.cs
+++ App/Service/AccountService.cs
@@ -2,18 +2,15 @@ using Microsoft.AspNetCore.Identity;
 using System.Security.Claims;
 namespace App.Service;
 
-public class AccountService : IAccountService
+public class AccountService(
+    UserManager<IdentityUser> userManager,
+    IUserStore<IdentityUser> userStore,
+    IUserEmailStore<IdentityUser> emailStore
+    ) : IAccountService
 {
-    private readonly UserManager<IdentityUser> _userManager;
-    private readonly IUserStore<IdentityUser> _userStore;
-    private readonly IUserEmailStore<IdentityUser> _emailStore;
-
-    public AccountService(UserManager<IdentityUser> userManager, IUserStore<IdentityUser> userStore)
-    {
-        _userManager = userManager;
-        _userStore = userStore;
-        _emailStore = GetEmailStore();
-    }
+    private readonly UserManager<IdentityUser> _userManager = userManager;
+    private readonly IUserStore<IdentityUser> _userStore = userStore;
+    private readonly IUserEmailStore<IdentityUser> _emailStore = emailStore;
 
     public async Task<IdentityResult> CreateDriverAccount(string email, string password)
     {
@@ -54,14 +51,5 @@ public class AccountService : IAccountService
         var user = await _userManager.GetUserAsync(securityInformation) 
             ?? throw new Exception("Can't find User using Claims Principal.");
         return await _userManager.GetEmailAsync(user) ?? throw new Exception("Can't find current user's email.");
-    }
-
-    private IUserEmailStore<IdentityUser> GetEmailStore()
-    {
-        if (!_userManager.SupportsUserEmail)
-        {
-            throw new NotSupportedException("The default UI requires a user store with email support.");
-        }
-        return (IUserEmailStore<IdentityUser>) _userStore;
     }
 }

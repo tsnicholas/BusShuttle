d8c1375c7f929c3235ac93df2e6355d0a0d10edd
tsnicholas <tsnicholas@bsu.edu>
2024-04-19 22:44:33 -0400

    Refactor unit tests to no longer use fixture

--- BusShuttleDatabase.Tests/Service/DatabaseServiceTest.cs
+++ BusShuttleDatabase.Tests/Service/DatabaseServiceTest.cs
@@ -1,8 +1,10 @@
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
 using BusShuttleModel;
 using Database.Service;
 namespace Database.Test.Service;
 
-public class DatabaseServiceTest : IClassFixture<DatabaseFixture>
+public class DatabaseServiceTest
 {
     private static readonly List<Bus> testBuses = [
         new Bus(1, 69),
@@ -24,51 +26,213 @@ public class DatabaseServiceTest : IClassFixture<DatabaseFixture>
         new Loop(1, "Ohio"),
         new Loop(2, "Canada")
     ];
-    private static List<Entry> testEntries = [
+    private static readonly List<Entry> testEntries = [
         new Entry(1, 5, 10).SetBus(testBuses[0]).SetDriver(testDrivers[0]).SetLoop(testLoops[0]).SetStop(testStops[0]),
         new Entry(2, 3, 1).SetBus(testBuses[1]).SetDriver(testDrivers[1]).SetLoop(testLoops[1]).SetStop(testStops[1])
     ];
-    private readonly DatabaseFixture _fixture;
-    private readonly BusShuttleContext context;
+    
+    private static readonly object _lock = new();
+    private static bool _isDatabaseCurrentlyBeingUsed;
+    private readonly BusShuttleContext context = CreateContext();
+
+    private static BusShuttleContext CreateContext()
+    {
+        var connection = new SqliteConnection(new SqliteConnectionStringBuilder{DataSource=":memory:"}.ToString());
+        return new BusShuttleContext(new DbContextOptionsBuilder<BusShuttleContext>().UseSqlite(connection).Options);
+    }
+
     private readonly DatabaseService service;
 
-    public DatabaseServiceTest(DatabaseFixture fixture)
+    public DatabaseServiceTest()
     {
-        _fixture = fixture;
-        Assert.NotNull(_fixture.Context);
-        context = _fixture.Context;
+        context.Database.EnsureDeleted();
+        context.Database.OpenConnection();
+        context.Database.EnsureCreated();
         service = new(context);
+    }
+
+    private void FillDatabase()
+    {
+        context.Buses.AddRange(testBuses);
+        context.Drivers.AddRange(testDrivers);
+        context.Stops.AddRange(testStops);
+        context.Routes.AddRange(testRoutes);
+        context.Loops.AddRange(testLoops);
+        context.Entries.AddRange(testEntries);
+        context.SaveChanges();
+    }
+
+    private void EmptyDatabase()
+    {
+        context.Buses.RemoveRange(context.Buses);
+        context.Drivers.RemoveRange(context.Drivers);
+        context.Stops.RemoveRange(context.Stops);
+        context.Routes.RemoveRange(context.Routes);
+        context.Loops.RemoveRange(context.Loops);
+        context.Entries.RemoveRange(context.Entries);
+        context.SaveChanges();
     }
 
     [Fact]
     public void Can_Get_All_Buses_Case()
     {
-        context.Buses.AddRange(testBuses);
-        context.SaveChanges();
+        FillDatabase();
         Assert.Equal(testBuses, service.GetAll<Bus>());
+        EmptyDatabase();
     }
 
     [Fact]
     public void Can_Get_All_Drivers_Case()
     {
-        context.Drivers.AddRange(testDrivers);
-        context.SaveChanges();
+        FillDatabase();
         Assert.Equal(testDrivers, service.GetAll<Driver>());
+        EmptyDatabase();
     }
 
     [Fact]
     public void Can_Get_All_Stops_Case()
     {
-        context.Stops.AddRange(testStops);
-        context.SaveChanges();
+        FillDatabase();
         Assert.Equal(testStops, service.GetAll<Stop>());
+        EmptyDatabase();
     }
 
     [Fact]
     public void Can_Get_All_Routes_Case()
     {
-        context.Routes.AddRange(testRoutes);
-        context.SaveChanges();
+        FillDatabase();
         Assert.Equal(testRoutes, service.GetAll<BusRoute>());
+        EmptyDatabase();
+    }
+
+    [Fact]
+    public void Can_Get_All_Loops_Case()
+    {
+        FillDatabase();
+        Assert.Equal(testLoops, service.GetAll<Loop>());
+        EmptyDatabase();
+    }
+
+    [Theory]
+    [InlineData(0, 1)]
+    [InlineData(1, 2)]
+    public void Can_Get_By_Id_Bus_Case(int index, int id)
+    {
+        FillDatabase();
+        Assert.Equal(testBuses[index], service.GetById<Bus>(id));
+        EmptyDatabase();
+    }
+
+    [Theory]
+    [InlineData(0, 1)]
+    [InlineData(1, 2)]
+    public void Can_Get_By_Id_Driver_Case(int index, int id)
+    {
+        FillDatabase();
+        Assert.Equal(testDrivers[index], service.GetById<Driver>(id));
+        EmptyDatabase();
+    }
+
+    [Theory]
+    [InlineData(0, 1)]
+    [InlineData(1, 2)]
+    public void Can_Get_By_Id_Stop_Case(int index, int id)
+    {
+        FillDatabase();
+        Assert.Equal(testStops[index], service.GetById<Stop>(id));
+        EmptyDatabase();
+    }
+
+    [Theory]
+    [InlineData(0, 1)]
+    [InlineData(1, 2)]
+    public void Can_Get_By_Id_Route_Case(int index, int id)
+    {
+        FillDatabase();
+        Assert.Equal(testRoutes[index], service.GetById<BusRoute>(id));
+        EmptyDatabase();
+    }
+
+    [Theory]
+    [InlineData(0, 1)]
+    [InlineData(1, 2)]
+    public void Can_Get_By_Id_Loop_Case(int index, int id)
+    {
+        FillDatabase();
+        Assert.Equal(testLoops[index], service.GetById<Loop>(id));
+        EmptyDatabase();
+    }
+
+    [Theory]
+    [InlineData(0, 1)]
+    [InlineData(1, 2)]
+    public void Can_Get_By_Id_Entry_Case(int index, int id)
+    {
+        lock (_lock)
+        {
+            if(!_isDatabaseCurrentlyBeingUsed)
+            {
+                return;
+            }
+        }
+        FillDatabase();
+        Assert.Equal(testEntries[index], service.GetById<Entry>(id));
+        EmptyDatabase();
+    }
+
+    [Fact]
+    public void Can_Create_Bus()
+    {
+        Bus bus = new(3, 90);
+        service.CreateEntity(bus);
+        Assert.Equal([bus], context.Buses.ToList());
+        EmptyDatabase();
+    }
+
+    [Fact]
+    public void Can_Create_Driver()
+    {
+        Driver driver = new(3, "Sally", "Johnson", "example@Gmail.com");
+        service.CreateEntity(driver);
+        Assert.Equal([driver], context.Drivers.ToList());
+        EmptyDatabase();
+    }
+
+    [Fact]
+    public void Can_Create_Stop()
+    {
+        Stop stop = new(3, "Mushroom Kingdom", 500, 0);
+        service.CreateEntity(stop);
+        Assert.Equal([stop], context.Stops.ToList());
+        EmptyDatabase();
+    }
+
+    [Fact]
+    public void Can_Create_Route()
+    {
+        BusRoute route = new BusRoute(3, 1).SetStop(new Stop(3, "Mushroom Kingdom", 500, 0));
+        service.CreateEntity(route);
+        Assert.Equal([route], context.Routes.ToList());
+        EmptyDatabase();
+    }
+
+    [Fact]
+    public void Can_Create_Loop()
+    {
+        Loop loop = new(3, "Green Hill Zone");
+        service.CreateEntity(loop);
+        Assert.Equal([loop], context.Loops.ToList());
+        EmptyDatabase();
+    }
+
+    [Fact]
+    public void Can_Create_Entry()
+    {
+        FillDatabase();
+        Entry entry = new Entry(3, 5, 10).SetBus(testBuses[0]).SetDriver(testDrivers[0]).SetStop(testStops[1]).SetLoop(testLoops[1]);
+        service.CreateEntity(entry);
+        var actual = context.Entries.OrderBy(entry => entry.Id).ToList();
+        Assert.Equal(entry, actual[2]);
+        EmptyDatabase();
     }
 }
